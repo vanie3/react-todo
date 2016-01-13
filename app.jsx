c27255b961a6c76358120e292d607a85eac76071
Vanessa Ceballos <Vanessa@Vanessas-MacBook-Pro.local>
2016-01-12 21:03:05 -0800

    adding input for color and changes color of div depending on what is typed in

--- app.jsx
+++ app.jsx
@@ -4,25 +4,44 @@ import ReactDOM from 'react-dom'
 class App extends React.Component {
     constructor(props) {
         super(props);
+        let { name, color } = props
 
-        this.state = { name: props.name }
+        this.state = { name, color }
     }
 
     updateName = (ev) => {
-        const { value } = ev.target;
+        let { value } = ev.target;
+
+        if (value.length === 0) {
+            value = this.props.name
+        }
 
         this.setState({ name: value })
     };
 
+    updateColor = (ev) => {
+        let { value } = ev.target;
+
+        if (value.length === 0) {
+            value = this.props.color
+        }
+
+        this.setState({ color: value })
+    };
+
     render() {
+        const { name, color } = this.state;
         return (
-            <div className='container' style={{color: 'red'}}>
+            <div className='container' style={{color}}>
                 <label>What's your name?</label>
-                <input type='text' onBlur={this.updateName}/>
+                <input type='text' onChange={this.updateName}/>
+                <label>What's your favorite color?</label>
+                <input type="text" defaultValue={color} onChange={this.updateColor}/>
                 <p>Your name is {this.state.name}</p>
+                <p>Your name is {this.state.color}</p>
             </div>
         )
     }
 }
 
-ReactDOM.render(<App name='Fred'/>, document.getElementById('react'));
+ReactDOM.render(<App name='Fred' color='red'/>, document.getElementById('react'));
